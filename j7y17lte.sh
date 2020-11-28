58761fa502d192edeeec1b9b5f50760a25fe38d8
Pietro Lotto <52181172+PietroLotto@users.noreply.github.com>
2020-11-27 21:18:35 -0300

    script

---
+++ j7y17lte.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/PietroLotto/android_device_skyhawk -b j7y17lte device/samsung/j7y17lte
+git clone https://gitlab.com/Universal7870/common/android_kernel_samsung_exynos7870.git -b skyhawk kernel/samsung/exynos7870
+. build/envsetup.sh
+lunch omni_j7y17lte-eng
+mka recoveryimage
